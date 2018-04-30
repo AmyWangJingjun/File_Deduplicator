9402026a692acd7b5cdc5119ad6579e00668b1c6
Jingjun Wang <jingjunw@bu.edu>
2018-04-29 22:52:56 -0400

    change argument mode

--- Dedu_V2_2/Entrance.java
+++ Dedu_V2_2/Entrance.java
@@ -18,24 +18,23 @@ public class Entrance{
 		if (args.length < 5) {
 			throw new IllegalArgumentException("Not a valid argument");
 		}
-		if (args[0].equals("Store")) {
-			//awake tocompact
+		if (args[0].equals("addfile")) {
+			newIns.newLock = 0;
 			newIns.stats = 0;
 		} else if (args[0].equals("Retrieve")){
-			// awake recover
+			newIns.newLock = -1;
 			newIns.stats = 1;
-		} else {
+		} else if (args[0].equals("setnewlocker")) {
+			newIns.newLock = 0;
+			newIns.stats = 0;
+		}
+		else {
 			throw new IllegalArgumentException("Not a valid argument: " + args[0]);
 		}
-		if (args[1].equals("-file")) {
-		
-		} else {
+		if (!args[1].equals("-file")) {
 			throw new IllegalArgumentException("Please input -file <yourfilepath>!");
 		}
-		
-		newIns.inputfile = args[2];
-		
-		File inputDir = new File(newIns.inputfile);
+		File inputDir = new File(args[2]);
 		if (newIns.stats == 0 && !inputDir.exists()) {
 			throw new IllegalArgumentException("File " +args[2] + "does not exist!");	
 		} 
@@ -46,40 +45,39 @@ public class Entrance{
 		if (!args[3].equals("-lock")) {
 			throw new IllegalArgumentException("Please input -lock <lockerpath>!");
 		}
-		
+
 		File lockDir = new File(args[4]);
-		newIns.outputfile = args[4];
-		if (newIns.stats == 1) {
-			if (!lockDir.isDirectory()) {
-				throw new IllegalArgumentException("Locker must be a valid Folder!");
+		if (newIns.newLock == 1 && newIns.stats == 0) {
+			if (lockDir.exists()) {
+				throw new  IllegalArgumentException(args[4] + " already exists!");
 			} else {
-				lockDir = new File(args[4] + "tmp/CP.SPLITER");			
-				if (!lockDir.exists()) {
-					throw new IllegalArgumentException("Not a valid locker!");
-				}
-			}
-		} else {
-			if (!lockDir.exists()) {
 				lockDir.mkdir();
-				newIns.newLock = 1;
 				System.out.println("Putting file " + args[2] + "into new locker " + args[4] + "...");
-			} else {
-				newIns.newLock = 0;
-				lockDir = new File(args[4] + "tmp/CP.SPLITER");			
-				if (!lockDir.exists()) {
-					throw new IllegalArgumentException("Not a valid locker!");
-				}
-				System.out.println("Putting file " + args[2] + "into existing locker " + args[4] + "...");
+			}		
+		} else if (newIns.newLock == 0) {
+			if (!lockDir.exists()) {
+				throw new  IllegalArgumentException(args[4] + "  is not a locker");
+			} 
+			lockDir = new File(args[4] + "tmp/CP.SPLITER");			
+			if (!lockDir.exists()) {
+				throw new IllegalArgumentException("Not a valid locker!");
 			}
 		}
-		
+		if (newIns.newLock == 0 && newIns.stats == 0) {
+			System.out.println("Putting file " + args[2] + "into existing locker " + args[4] + "...");
+		}
+
 		if (newIns.stats == 1) {
 			if (args.length < 6) {
 				throw new IllegalArgumentException("Please input file name you want to retrieve!");
 			} else if (!(args[5].equals("all") || new File(args[4] + args[5]).exists())) {
-					throw new IllegalArgumentException("File does not exist");
+				throw new IllegalArgumentException("File does not exist");
 			}
 		}
+		
+		newIns.inputfile = args[2];
+		newIns.outputfile = args[4];
+		
 		Zip_Lib lib = new Zip_Lib(newIns.inputfile, newIns.outputfile);
 		if (newIns.stats == 0) {
 			lib.toCompact(newIns.newLock);
